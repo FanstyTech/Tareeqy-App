36720f6d06e52a00fd9a9d40aa587e5232cc0840
HusamFarajallah <59307399+HusamFarajallah@users.noreply.github.com>
2023-01-26 15:03:09 +0200

    init

--- ClientApp/src/components/Card/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as ProductCard } from "./ProductCard";
-export { default as CardList } from "./cardList";
-export { default as ProductFilterSidebar } from "./shopFilterSidebar";
-export { default as ProductSort } from "./ProductSort";
-export { default as ProductCartWidget } from "./cartWidget";
--- ClientApp/src/x.js
+++
@@ -1,8 +0,0 @@
-import SpinnerComp from "./components/Spinner";
-
-{ loading && <SpinnerComp/>}
-
-const [loading, setLoading] = useState<boolean>(false);
-
-    setLoading(false)
-    setLoading(true)
